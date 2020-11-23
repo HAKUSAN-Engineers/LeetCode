00eeafa0dcd9992debd9e7c296f23ed272a48fb7
Yusuke <yhakamaya1031@gmail.com>
2020-11-23 14:35:54 +0900

    added a solution

---
+++ 1395.c
@@ -0,0 +1,18 @@
+int numTeams(int* rating, int ratingSize)
+{
+    int count = 0;
+
+    for (int i = 0; i < ratingSize - 2; i++)
+    {
+        for (int j = i + 1; j < ratingSize - 1; j++)
+        {
+            if (rating[i] > rating[j])
+                for (int k = j + 1; k < ratingSize; k++)
+                    count += (rating[j] > rating[k]) ? 1 : 0;
+            if (rating[i] < rating[j])
+                for (int k = j + 1; k < ratingSize; k++)
+                    count += (rating[j] < rating[k]) ? 1 : 0;
+        }
+    }
+    return count;
+}
